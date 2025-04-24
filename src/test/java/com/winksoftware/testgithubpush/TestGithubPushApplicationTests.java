7a369701f057a546e37f25682f8c9e712cc5e767
cgalgali <chetan.galgali@downloadwink.com>
2025-04-24 16:56:37 -0400

    first git-action

--- src/test/java/com/winksoftware/testgithubpush/TestGithubPushApplicationTests.java
+++ src/test/java/com/winksoftware/testgithubpush/TestGithubPushApplicationTests.java
@@ -1,5 +1,6 @@
 package com.winksoftware.testgithubpush;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,11 @@ class TestGithubPushApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failTest() {
+		Assertions.fail("This is a failed test");
 	}
 
 }
