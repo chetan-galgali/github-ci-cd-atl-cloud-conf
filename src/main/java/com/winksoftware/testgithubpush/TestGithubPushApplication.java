23fb799f86a5aa586f5920d4c7511b698ed59886
cgalgali <chetan.galgali@downloadwink.com>
2025-04-24 15:58:13 -0400

    Initial commit

---
+++ src/main/java/com/winksoftware/testgithubpush/TestGithubPushApplication.java
@@ -0,0 +1,13 @@
+package com.winksoftware.testgithubpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winksoftware/testgithubpush/TestGithubPushApplicationTests.java
@@ -0,0 +1,14 @@
+package com.winksoftware.testgithubpush;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
